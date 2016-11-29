40856937f7e48899b5a1825346a4a58a5313c3a0
deadking01 <wuqingfeng234@163.com>
2016-11-29 10:06:39 +0800

    更换dubbo版本

--- container/tomcat/tomcat-consumer/src/main/java/com/git/wuqf/dubbo/demo/tomcat/consumer/UserController.java
+++ container/tomcat/tomcat-consumer/src/main/java/com/git/wuqf/dubbo/demo/tomcat/consumer/UserController.java
@@ -5,6 +5,7 @@ import com.git.wuqf.dubbo.demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by wuqf on 16-11-17.
@@ -16,8 +17,10 @@ public class UserController {
     @Autowired
     private DemoService demoService;
 
-
-    public User updateUser(User user){
+    @RequestMapping(value = "/user")
+    public @ResponseBody
+    User updateUser(){
+        User user=new User(1,"wuqf",4);
         return demoService.updateUserInfo(user);
     }
 }
