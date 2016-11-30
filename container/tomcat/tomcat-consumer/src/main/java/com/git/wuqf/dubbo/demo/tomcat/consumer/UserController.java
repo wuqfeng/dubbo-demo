be83fdcb7f1e0987baa63269ca773599f84c8aa0
deadking01 <wuqingfeng234@163.com>
2016-11-30 15:54:40 +0800

    增加测试内容

--- container/tomcat/tomcat-consumer/src/main/java/com/git/wuqf/dubbo/demo/tomcat/consumer/UserController.java
+++ container/tomcat/tomcat-consumer/src/main/java/com/git/wuqf/dubbo/demo/tomcat/consumer/UserController.java
@@ -1,11 +1,15 @@
 package com.git.wuqf.dubbo.demo.tomcat.consumer;
 
+import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.git.wuqf.dubbo.demo.api.DemoService;
 import com.git.wuqf.dubbo.demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by wuqf on 16-11-17.
@@ -21,6 +25,10 @@ public class UserController {
     public @ResponseBody
     User updateUser(){
         User user=new User(1,"wuqf",4);
-        return demoService.updateUserInfo(user);
+        user= demoService.updateUserInfo(user);
+        boolean isConsumerSide = RpcContext.getContext().isConsumerSide();
+        String serverPort=RpcContext.getContext().getRemoteAddressString();
+        List<URL> application=RpcContext.getContext().getUrls();
+        return user;
     }
 }
