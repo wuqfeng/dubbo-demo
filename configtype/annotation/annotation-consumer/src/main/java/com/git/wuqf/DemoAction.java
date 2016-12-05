13665167a979b44422a1ead5df14e64f90ce8b34
deadking01 <wuqingfeng234@163.com>
2016-12-05 09:16:05 +0800

    xml配置更改为注解

--- configtype/annotation/annotation-consumer/src/main/java/com/git/wuqf/DemoAction.java
+++ configtype/annotation/annotation-consumer/src/main/java/com/git/wuqf/DemoAction.java
@@ -20,19 +20,4 @@ public class DemoAction {
     public void setDemoService(DemoService demoService) {
         this.demoService = demoService;
     }
-
-    public void start() throws Exception {
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {
-            try {
-                String hello = demoService.sayHello("world" + i);
-                System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] " + hello);
-                User user=new User(i,"wuqf",11);
-                user= demoService.updateUserInfo(user);
-                System.out.print(user.toString());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            Thread.sleep(2000);
-        }
-    }
 }
